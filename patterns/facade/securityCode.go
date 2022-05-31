7a2c418ac089bc28c8e1fd158fd9a33bbdc7b06c
Renato Spakauskas <renatospaka@gmail.com>
2022-05-31 10:54:33 -0300

    feat: :sparkles: facade pattern
    small implementation of the facade design pattern

---
+++ patterns/facade/securityCode.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type securityCode struct {
+	code int
+}
+
+func newSecurityCode(code int) *securityCode {
+	return &securityCode{
+		code: code,
+	}
+}
+
+func (s *securityCode) checkCode(incomingCode int) error {
+	if s.code != incomingCode {
+		return fmt.Errorf("security code is incorrect")
+	}
+	fmt.Println("security code verified")
+	return nil
+}
