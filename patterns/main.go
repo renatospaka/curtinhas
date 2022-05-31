6f246688ede9b5ec2bbd8829745939823fa91000
Renato Spakauskas <renatospaka@gmail.com>
2022-05-31 15:31:00 -0300

    feat: :sparkles: abstract factory pattern
    small implementation of the abstract factory design pattern

--- patterns/main.go
+++ patterns/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	abstractFactory "github.com/renatospaka/design-patterns/abstractFactory"
 	"github.com/renatospaka/design-patterns/facade"
 )
 
@@ -22,4 +23,38 @@ func main() {
 	if err != nil {
 		log.Fatalf("error: %s\n", err.Error())
 	}
+	fmt.Println("==================================")
+	fmt.Println()
+
+	fmt.Println("This is the abstract factory implementation")
+	adidasFactory, _ := abstractFactory.GetSportsFactory("adidas")
+	nikeFactory, _ := abstractFactory.GetSportsFactory("nike")
+
+	nikeShoe := nikeFactory.MakeShoe()
+	nikeShirt := nikeFactory.MakeShirt()
+
+	adidasShoe := adidasFactory.MakeShoe()
+	adidasShirt := adidasFactory.MakeShirt()
+
+	printShoeDetails(nikeShoe)
+	printShirtDetails(nikeShirt)
+
+	printShoeDetails(adidasShoe)
+	printShirtDetails(adidasShirt)
+	fmt.Println("==================================")
+	fmt.Println()
+}
+
+func printShoeDetails(s abstractFactory.IShoe) {
+	fmt.Printf("Logo: %s", s.GetLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.GetSize())
+	fmt.Println()
+}
+
+func printShirtDetails(s abstractFactory.IShirt) {
+	fmt.Printf("Logo: %s", s.GetLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.GetSize())
+	fmt.Println()
 }
