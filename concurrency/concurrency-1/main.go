7296a682b41890dac12f9062c7fd8bc78b0c1478
Renato Spakauskas <renatospaka@gmail.com>
2023-07-10 15:39:50 -0300

    feat: :sparkles: doSomething method
    there is no pointer to the lock

---
+++ concurrency/concurrency-1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type User struct {
+	lock sync.RWMutex
+	Name string
+}
+
+func doSomething(u User) {
+	u.lock.RLock()
+	defer u.lock.RUnlock()
+
+	// do something with `u`
+	u.Name = "Name"
+	fmt.Printf("Other name: %s\n", u.Name)
+}
+
+func main() {
+	u := User{Name: "John"}
+	doSomething(u)
+	fmt.Printf("Name: %s\n", u.Name)
+}
