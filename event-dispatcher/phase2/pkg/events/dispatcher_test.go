446391ecde295e56e06fce5f3705b39804500526
Renato Spakauskas <renatospaka@gmail.com>
2024-06-23 13:15:41 -0300

    test: :white_check_mark: testing registration of an event

---
+++ event-dispatcher/phase2/pkg/events/dispatcher_test.go
@@ -0,0 +1,76 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+	ID int
+}
+
+func (h *TestEventHandler) Handle(event EventInterface) {
+}
+
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event1          TestEvent
+	event2          TestEvent
+	handler1        TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (suite *EventDispatcherTestSuite) SetupTest() {
+	suite.eventDispatcher = NewEventDispatcher()
+	suite.handler1 = TestEventHandler{
+		ID: 1,
+	}
+	suite.handler2 = TestEventHandler{
+		ID: 2,
+	}
+	suite.handler3 = TestEventHandler{
+		ID: 3,
+	}
+	suite.event1 = TestEvent{Name: "event1", Payload: "payload1"}
+	suite.event2 = TestEvent{Name: "event2", Payload: "payload2"}
+	suite.eventDispatcher = &EventDispatcher{handlers: make(map[string][]EventHandlerInterface)}
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	err := suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	assert.Equal(suite.T(), &suite.handler1, suite.eventDispatcher.handlers[suite.event1.GetName()][0])
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event1.GetName()][1])
+}
+
+func TestSite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}
